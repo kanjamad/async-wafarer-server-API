0013d6ce4de11898308867f14f332e70f2e137f9
kanjamad bosel <kanjamadpor@gmail.com>
2019-05-07 21:11:34 -0700

    Adds user, city, and post models

---
+++ models/post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    city_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'City'
+    },
+    title: String,
+    body: String,
+    image_url: String,
+    date_created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+module.exports = Post;
